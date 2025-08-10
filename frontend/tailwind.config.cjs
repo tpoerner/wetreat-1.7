c67dd4bfabf522f2cd28dc739f2f629d95895243
tpoerner <tpoerner@users.noreply.github.com>
2025-08-10 22:56:00 +0200

    Update tailwind.config.cjs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,1 +1,5 @@
-module.exports = { content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], theme: { extend: {} }, plugins: [] };
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: []
+};
